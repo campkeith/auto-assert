65c2fa1755a6838002f4f6a3d3227b7c1879868c
Keith Campbell <kacampb2@illinois.edu>
2015-04-17 00:08:32 -0500

    Complete (compiles, but otherwise untested) implementation of auto-assert pass.

--- passes/AutoAssert.cpp
+++ passes/AutoAssert.cpp
@@ -15,6 +15,7 @@ namespace {
 static const BinaryOps shift_opcodes[] = {Instruction::Shl, Instruction::LShr, Instruction::AShr, Instruction::BinaryOpsEnd};
 static const BinaryOps divrem_opcodes[] = {Instruction::UDiv, Instruction::URem,
                                            Instruction::SDiv, Instruction::SRem, Instruction::BinaryOpsEnd};
+static const BinaryOps sdivrem_opcodes[] = {Instruction::SDiv, Instruction::SRem, Instruction::BinaryOpsEnd};
 
 static bool opcode_in_set(BinaryOps opcode, const BinaryOps set[])
 {
@@ -39,18 +40,18 @@ struct AutoAssertPass : ModulePass
 
     bool runOnModule(Module & module)
     {
-        createAssertPrototype(&module);
-        insertAssertions(&module);
+        createAssertFunctionPrototype(&module);
+        createAssertions(&module);
         return true;
     }
 
-    void createAssertPrototype(Module * module)
+    void createAssertFunctionPrototype(Module * module)
     {
         FunctionType * func_type = FunctionType::get(Type::getVoidTy(context), Type::getInt1Ty(context), false);
         assert_func = cast<Function>(module->getOrInsertFunction("assert", func_type));
     }
 
-    void insertAssertions(Module * module)
+    void createAssertions(Module * module)
     {
         for each(function, *module)
         {
@@ -59,15 +60,16 @@ struct AutoAssertPass : ModulePass
                 for each(inst, *block)
                 {
                     cursor = inst;
-                    handleInstruction(block, inst);
+                    createAssertionsForInstruction(inst);
                 }
             }
         }
     }
 
-    void handleInstruction(BasicBlock * block, Instruction * inst)
+    void createAssertionsForInstruction(Instruction * inst)
     {
         BinaryOperator * bin_op = dyn_cast<BinaryOperator>(inst);
+        GetElementPtrInst * gep = dyn_cast<GetElementPtrInst>(inst);
         if (bin_op)
         {
             BinaryOps opcode = bin_op->getOpcode();
@@ -81,30 +83,38 @@ struct AutoAssertPass : ModulePass
             }
             if (opcode_in_set(opcode, divrem_opcodes))
             {
-                assertDivRemNonzero(bin_op);
+                assertNoDivRemByZero(bin_op);
             }
+            if (opcode_in_set(opcode, sdivrem_opcodes))
+            {
+                assertNoSDivRemOverflow(bin_op);
+            }
+        }
+        else if (gep && gep->isInBounds())
+        {
+            assertGetElementPtrInBounds(gep);
         }
     }
 
-    void assertShiftInBounds(BinaryOperator * inst)
+    void assertShiftInBounds(BinaryOperator * shift)
     {
-        IntegerType * type = cast<IntegerType>(inst->getType());
+        IntegerType * type = cast<IntegerType>(shift->getType());
         ConstantInt * shift_limit = ConstantInt::get(type, type->getBitWidth());
-        createAssertion(new ICmpInst(cursor, CmpInst::ICMP_ULT, inst->getOperand(1), shift_limit));
+        createAssertion(new ICmpInst(cursor, CmpInst::ICMP_ULT, shift->getOperand(1), shift_limit));
     }
 
-    void assertNoSignedWrap(BinaryOperator * inst)
+    void assertNoSignedWrap(BinaryOperator * arith)
     {
-        BinaryOps opcode = inst->getOpcode();
-        unsigned width = cast<IntegerType>(inst->getType())->getBitWidth();
+        BinaryOps opcode = arith->getOpcode();
+        unsigned width = cast<IntegerType>(arith->getType())->getBitWidth();
         unsigned new_width = opcode == Instruction::Add ? width + 1
                            : opcode == Instruction::Sub ? width + 1
                            : opcode == Instruction::Mul ? 2 * width
                            : opcode == Instruction::Shl ? 2 * width - 1
                            : (llvm_unreachable("assertNoSignedWrap: unexpected opcode"), 0);
         IntegerType * new_type = IntegerType::get(context, new_width);
-        SExtInst * operands[2] = { new SExtInst(inst->getOperand(0), new_type, "", cursor),
-                                   new SExtInst(inst->getOperand(1), new_type, "", cursor) };
+        SExtInst * operands[2] = { new SExtInst(arith->getOperand(0), new_type, "", cursor),
+                                   new SExtInst(arith->getOperand(1), new_type, "", cursor) };
         BinaryOperator * new_op = BinaryOperator::Create(opcode, operands[0], operands[1], "", cursor);
         Constant * min_value = ConstantInt::get(new_type, APInt::getSignedMinValue(width));
         Constant * max_value = ConstantInt::get(new_type, APInt::getSignedMaxValue(width));
@@ -112,10 +122,43 @@ struct AutoAssertPass : ModulePass
         createAssertion(new ICmpInst(cursor, CmpInst::ICMP_SLE, new_op, max_value));
     }
 
-    void assertDivRemNonzero(BinaryOperator * inst)
+    void assertNoDivRemByZero(BinaryOperator * divrem)
     {
-        Constant * zero = ConstantInt::get(inst->getType(), 0);
-        createAssertion(new ICmpInst(cursor, CmpInst::ICMP_NE, inst->getOperand(1), zero));
+        Constant * zero = ConstantInt::get(divrem->getType(), 0);
+        createAssertion(new ICmpInst(cursor, CmpInst::ICMP_NE, divrem->getOperand(1), zero));
+    }
+    
+    void assertNoSDivRemOverflow(BinaryOperator * sdivrem)
+    {
+        IntegerType * type = cast<IntegerType>(sdivrem->getType());
+        unsigned width = type->getBitWidth();
+        Constant * min_value = ConstantInt::get(sdivrem->getType(), APInt::getSignedMinValue(width));
+        Constant * minus_one = ConstantInt::getSigned(type, -1);
+        ICmpInst * dividend_pred = new ICmpInst(cursor, CmpInst::ICMP_NE, sdivrem->getOperand(0), min_value);
+        ICmpInst *  divisor_pred = new ICmpInst(cursor, CmpInst::ICMP_NE, sdivrem->getOperand(1), minus_one);
+        createAssertion(BinaryOperator::Create(Instruction::Or, dividend_pred, divisor_pred, "", cursor));
+    }
+
+    void assertGetElementPtrInBounds(GetElementPtrInst * gep)
+    {
+        Value * base_pointer = gep->getOperand(0);
+        if (isa<GlobalVariable>(base_pointer) || isa<AllocaInst>(base_pointer))
+        {
+            Value * first_index = gep->getOperand(1);
+            Constant * zero = ConstantInt::get(first_index->getType(), 0);
+            createAssertion(new ICmpInst(cursor, CmpInst::ICMP_EQ, first_index, zero));
+        }
+        SequentialType * type = gep->getType();
+        for (unsigned index = 2; index < gep->getNumOperands(); index++)
+        {
+            Value * operand = gep->getOperand(index);
+            type = cast<SequentialType>(type->getElementType());
+            unsigned size = cast<ArrayType>(type)->getNumElements();
+            Constant * zero = ConstantInt::getSigned(operand->getType(), 0);
+            Constant * index_limit = ConstantInt::getSigned(operand->getType(), size);
+            createAssertion(new ICmpInst(cursor, CmpInst::ICMP_SGE, operand, zero));
+            createAssertion(new ICmpInst(cursor, CmpInst::ICMP_SLT, operand, index_limit));
+        }
     }
 
     void createAssertion(Value * predicate)
