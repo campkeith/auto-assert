0fda44f1dc8c3ff77b237564510f7b9f9e2fe5c3
Keith Campbell <kacampb2@illinois.edu>
2015-05-03 23:18:07 -0500

    Extend assertion pruning pass to remove multiple assertions with the same predicate.

--- passes/PruneAsserts.cpp
+++ passes/PruneAsserts.cpp
@@ -4,41 +4,62 @@
 #include <llvm/Constants.h>
 #include <llvm/BasicBlock.h>
 #include <llvm/Instructions.h>
+#include <set>
 
 using namespace llvm;
 using std::vector;
+using std::set;
 namespace {
 
-struct PruneAssertsPass : BasicBlockPass
+struct PruneAssertsPass : FunctionPass
 {
     static char ID;
-    PruneAssertsPass() : BasicBlockPass(ID) {}
+    PruneAssertsPass() : FunctionPass(ID) {}
 
-    bool runOnBasicBlock(BasicBlock & block)
+    bool runOnFunction(Function & function)
     {
-        vector<CallInst *> const_asserts;
-        for each(inst, block)
+        set<Value *> assert_preds;
+        vector<CallInst *> delete_list;
+        for each(block, function)
         {
-            CallInst * call = dyn_cast<CallInst>(inst);
-            if (call && call->getCalledFunction()->getName() == "assert")
+            for each(inst, *block)
             {
-                ConstantInt * const_arg = dyn_cast<ConstantInt>(call->getArgOperand(0));
-                if (const_arg)
+                CallInst * call = dyn_cast<CallInst>(inst);
+                if (call && call->getCalledFunction()->getName() == "assert")
                 {
-                    assert(const_arg->isOne());
-                    const_asserts.push_back(call);
+                    pruneAssert(call, assert_preds, delete_list);
                 }
             }
         }
-        for each(assert_call, const_asserts)
+        for each(assert_call, delete_list)
         {
             (*assert_call)->eraseFromParent();
         }
-        return !const_asserts.empty();
+        return !delete_list.empty();
+    }
+
+    void pruneAssert(CallInst * assert_call, set<Value *> & assert_preds, vector<CallInst *> & delete_list)
+    {
+        Value * predicate = assert_call->getArgOperand(0);
+        ConstantInt * const_pred = dyn_cast<ConstantInt>(predicate);
+        if (const_pred)
+        {
+            assert(const_pred->isOne());
+            delete_list.push_back(assert_call);
+        }
+        else
+        {
+            bool inserted = assert_preds.insert(predicate).second;
+            if (!inserted)
+            {
+                delete_list.push_back(assert_call);
+            }
+        }
     }
 };
 char PruneAssertsPass::ID;
 
-static RegisterPass<PruneAssertsPass> pass("prune-asserts", "Prune assertions with a constant predicate");
+static RegisterPass<PruneAssertsPass> pass("prune-asserts",
+        "Prune redundant assertions and assertions with a constant predicate");
 
 }
